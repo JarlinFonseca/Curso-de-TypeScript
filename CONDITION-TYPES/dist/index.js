928fad052b1bd1f280cf2026caa2c8062f460636
Jarlin Fonseca <j.fonseca@nelumbo.com.co>
2024-04-25 16:20:20 -0500

    Conditional Types (Tipos Condicionales) | Clase 5 | Curso TypeScript

---
+++ CONDITION-TYPES/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function seleccionAnimal(animal) {
+    console.log(animal);
+}
+seleccionAnimal({
+    nombrePerro: 'Toby',
+    edad: 5,
+    ladra: true
+});
---
+++ CONDITION-TYPES/index.ts
@@ -0,0 +1,24 @@
+﻿type Perro = {
+    nombrePerro: string;
+    edad: number;
+    ladra: boolean;
+}
+
+type Gato = {
+    nombreGato: string;
+    edad: number;
+    tepra: boolean;
+}
+
+type SeleccionAnimnal<T extends 'perro' | 'gato'> = 
+T extends 'perro' ? Perro : Gato;
+
+function seleccionAnimal<T extends 'perro' | 'gato'>(animal: SeleccionAnimnal<T>) {
+    console.log(animal);
+}
+
+seleccionAnimal< 'perro' >({
+    nombrePerro: 'Toby',
+    edad: 5,
+    ladra: true
+})
